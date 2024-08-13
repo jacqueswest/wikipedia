4d5014e1f87f0fafc2abaf4039ea01c7111b48b1
Jacques van der Westhuizen <jacqueswest@gmail.com>
2024-08-13 12:10:30 +0200

    small changes

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,6 +7,7 @@ class BasePage:
         self.driver = driver
 
     def wait_for_element(self, by, locator, timeout=10):
+        """Wait for the element and return it."""
         return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by, locator)))
 
     def wait_for_element_to_be_clickable(self, locator, timeout=10):
